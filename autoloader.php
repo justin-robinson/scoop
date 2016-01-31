b3bd49214f249eeef66920fc1d7221d3d93d0404
justinrobinson <justinrobinson@bizjournals.com>
2016-01-31 18:10:49 -0500

    [master] optimize autoloading speed

--- autoloader.php
+++ autoloader.php
@@ -4,30 +4,19 @@ require_once $_SERVER['R_DOCUMENT_ROOT'] . '/phpr/config.php';
 require_once $_SERVER['R_DOCUMENT_ROOT'] . '/phpr/path.php';
 
 /**
- * @param $fullClassPath
+ * @param $className
  */
-function r_autoloader ( $fullClassPath ) {
+function r_autoloader ( $className ) {
 
     // class names can be uppercase but files are lower case
-    $fullClassPath = strtolower ( $fullClassPath );
+    $relativeFilePath = '/'
+        . str_replace ( '\\', '/', strtolower ( $className ) )
+        . '.php';
 
-    // an array containing each folder to get to the file
-    $foldersArray = explode ( '\\', $fullClassPath );
-
-    // last item is the file and class name
-    $className = array_pop ( $foldersArray );
-
-    // glue folders together
-    $folders = implode ( '/', $foldersArray );
-
-    // full path to the file
-    $relativePath = '/' . $folders . '/' . $className . '.php';
-
-    $fileExists = false;
     foreach ( \phpr\Config::get_class_paths () as $classPath ) {
-        $filepath = $classPath . $relativePath;
+        $filepath = $classPath . $relativeFilePath;
 
-        if ( $fileExists = file_exists ( $filepath ) ) {
+        if ( file_exists ( $filepath ) ) {
             include_once ( $filepath );
             break;
         }
@@ -35,8 +24,9 @@ function r_autoloader ( $fullClassPath ) {
 }
 
 $composerAutoloaderPath = $_SERVER['R_DOCUMENT_ROOT'] . '/vendor/autoload.php';
-if ( file_exists($composerAutoloaderPath) ) {
+if ( file_exists ( $composerAutoloaderPath ) ) {
     require_once $composerAutoloaderPath;
 }
-spl_autoload_register ( 'r_autoloader' );
+
+spl_autoload_register ( 'r_autoloader', true, true );
 
