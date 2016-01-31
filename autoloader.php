944006813d8c46c3678a5582a1ce3b2713c07137
justinrobinson <justinrobinson@bizjournals.com>
2016-01-31 18:43:12 -0500

    [master] even leaner autoloader

--- autoloader.php
+++ autoloader.php
@@ -1,37 +1,29 @@
 <?php
 
-require_once $_SERVER['R_DOCUMENT_ROOT'] . '/phpr/config.php';
-require_once $_SERVER['R_DOCUMENT_ROOT'] . '/phpr/path.php';
-
-/**
- * @param $className
- */
-function r_autoloader ( $className ) {
-
-    // class names can be uppercase but files are lower case
-    // namespace can have \, but we need / for the file path
-    $relativeFilePath = '/'
-        . str_replace ( '\\', '/', strtolower ( $className ) )
-        . '.php';
-
-    // looks in
-    // 1. site classpath
-    // 2. shared site classpath
-    // 3. phpr classpath
-    foreach ( \phpr\Config::get_class_paths () as $classPath ) {
-        $filepath = $classPath . $relativeFilePath;
-
-        if ( file_exists ( $filepath ) ) {
-            include_once ( $filepath );
-            break;
-        }
-    }
-}
-
+/**************
+ * composer
+ * ************/
 $composerAutoloaderPath = $_SERVER['R_DOCUMENT_ROOT'] . '/vendor/autoload.php';
 if ( file_exists ( $composerAutoloaderPath ) ) {
     require_once $composerAutoloaderPath;
 }
 
-spl_autoload_register ( 'r_autoloader', true, true );
+/**************
+ * phpr
+ **************/
+require_once $_SERVER['R_DOCUMENT_ROOT'] . '/phpr/config.php';
+require_once $_SERVER['R_DOCUMENT_ROOT'] . '/phpr/path.php';
+
+// load all classpaths that exist
+foreach ( \phpr\Config::get_class_paths () as $classPath ) {
+    if ( $classPath = realpath ( $classPath ) ) {
+        set_include_path ( get_include_path () . PATH_SEPARATOR . $classPath . '/' );
+    }
+}
+
+spl_autoload_register (
+    function ( $className ) {
+
+        spl_autoload ( $className );
+    }, true, true );
 
