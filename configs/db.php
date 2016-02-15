615053868ea1ff2414f970e1edb0066328bd8328
justinrobinson <justinrobinson@bizjournals.com>
2016-02-15 15:30:06 -0500

    [master] add benchmark option to scoop script

--- configs/db.php
+++ configs/db.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'host'     => 'localhost',
-    'user'     => 'root',
-    'password' => '',
+    'host'     => 'jor.pw',
+    'user'     => 'test',
+    'password' => 'test',
     'port'     => ' 3306',
 ];
---
+++ db/jorpw/test.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DB\JorPw;
+
+/**
+ * Class Test
+ * @package DB\JorPw
+ * @author jrobinson (robotically)
+ * @date 2016/02/15
+ * @inheritdoc
+ * This file is only generated once
+ * Put your class specific code in here
+ */
+class Test extends \DBCore\JorPw\Test {
+
+
+
+
+
+
+}
+
+?>
