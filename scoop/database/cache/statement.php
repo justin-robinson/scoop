6eaeea81471bc89647fed236dfa2ac7c0e2e1a20
justinrobinson <justinrobinson@bizjournals.com>
2016-05-14 21:12:38 -0400

    [php56] fix broken exists function in statement cache

--- scoop/database/cache/statement.php
+++ scoop/database/cache/statement.php
@@ -40,7 +40,7 @@ class Statement {
      */
     public function exists ( $key ) {
 
-        return is_null($this->cache->get($key));
+        return !is_null($this->cache->get($key));
     }
 
     /**
