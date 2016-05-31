f9bbcd0e1129007ca3fb2d01cf42ef89f9b5fced
justinrobinson <justinrobinson@bizjournals.com>
2016-05-31 10:16:57 -0400

    [master] fix null values being marked as not set

--- scoop/database/model/generic.php
+++ scoop/database/model/generic.php
@@ -115,7 +115,7 @@ class Generic implements \JsonSerializable {
      */
     public function __isset ( $name ) {
 
-        return isset($this->dBValuesArray[$name]);
+        return array_key_exists($name, $this->dBValuesArray);
     }
 
     /**
