0241f0a0e65633cafa7398d08f13ddfc967e0f9f
justinrobinson <justinrobinson@bizjournals.com>
2016-06-03 16:16:15 -0400

    [master] more code coverage

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -7,6 +7,18 @@ use Scoop\Config;
  */
 class ConfigTest extends PHPUnit_Framework_TestCase {
 
+    public function test_get_db_config () {
+
+        $expected = [
+            'host'     => 'jor.pw',
+            'user'     => 'scoop',
+            'password' => '',
+            'port'     => '3306',
+        ];
+
+        $this->assertEquals($expected, Config::get_db_config());
+    }
+
     public function test_option_exists () {
 
         $value = rand(1,99999);
@@ -20,5 +32,20 @@ class ConfigTest extends PHPUnit_Framework_TestCase {
         $returnedValue = Config::get_option($optionName);
         $this->assertEquals($value, $returnedValue, "{$optionName} should equal {$value}, got {$returnedValue}");
 
+    }
+    
+    public function test_set_options () {
+        
+        $options = [
+            'dummy1' => 'value1',
+            'dummy2' => 'value2',
+            'dummy3' => 'value3',
+        ];
+        
+        Config::set_options($options);
+        
+        $this->assertEquals('value1', Config::get_option('dummy1'));
+        $this->assertEquals('value2', Config::get_option('dummy2'));
+        $this->assertEquals('value3', Config::get_option('dummy3'));
     }
 }
