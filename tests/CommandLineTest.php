f84b9de88c5de955f343bec83857de8d74060422
justinrobinson <justinrobinson@bizjournals.com>
2016-06-03 16:31:23 -0400

    [master] more tests

--- tests/CommandLineTest.php
+++ tests/CommandLineTest.php
@@ -16,14 +16,16 @@ class CommandLineTest extends PHPUnit_Framework_TestCase {
             '-o',
             'short-opt-value',
             '--long-opt',
-            'long-opt-value'
+            'long-opt-value',
+            '--long=equals'
         ];
 
         $expected = [
             'one',
             'two',
             'o' => 'short-opt-value',
-            'long-opt' => 'long-opt-value'
+            'long-opt' => 'long-opt-value',
+            'long' => 'equals'
         ];
 
         $this->assertEquals($expected, CommandLine::parseArgs($argv));
@@ -38,7 +40,11 @@ class CommandLineTest extends PHPUnit_Framework_TestCase {
             '-b',
             'false',
             '-i',
-            1
+            1,
+            '-B',
+            true,
+            '-a',
+            'pop'
         ];
 
         CommandLine::parseArgs($argv);
@@ -46,5 +52,9 @@ class CommandLineTest extends PHPUnit_Framework_TestCase {
         $this->assertTrue( CommandLine::getBoolean( 's' ), "cli arg value 'y' should be returned as true");
         $this->assertFalse( CommandLine::getBoolean( 'b' ), "cli arg value 'false' should be returned as false" );
         $this->assertTrue( CommandLine::getBoolean( 'i' ), "cli arg value '1' should be returned as true");
+        $this->assertTrue( CommandLine::getBoolean( 'B' ), "cli arg value 'B' should be returned as true");
+        $this->assertFalse( CommandLine::getBoolean( 'dne' ), "cli arg value 'dne' should be returned as false");
+        $this->assertEquals('default', CommandLine::getBoolean( 'a', 'default' ), "cli arg value 'a' should be return the default value");
+
     }
 }
