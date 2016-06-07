c6f178fd565b1849650220b19e7756b387ecb236
justinrobinson <justinrobinson@bizjournals.com>
2016-06-07 11:49:06 -0400

    [php56] query buffer tests

--- tests/database/query/BufferTest.php
+++ tests/database/query/BufferTest.php
@@ -42,6 +42,10 @@ class BufferTest extends PHPUnit_Framework_TestCase {
 
         $this->expectException( Exception::class );
         new Buffer($this->maxSize, stdClass::class, "buffer should throw an error is the model class isn't a scoop model");
+
+        $buffer = new Buffer($this->maxSize, Test::class);
+
+        $this->assertInstanceOf(Buffer::class, $buffer);
     }
 
     public function test_insert () {
@@ -56,6 +60,10 @@ class BufferTest extends PHPUnit_Framework_TestCase {
             $this->assertNotEmpty( $test->id, "inserting more than rows than the max size should trigger a flush" );
             $test->delete();
         }
+
+        $buffer = new Buffer(0, Test::class);
+        $test = new Test();
+        $this->assertFalse($buffer->insert($test), "inserting to a zero sized buffer should fail");
     }
 
     public function test_flush () {
@@ -74,6 +82,21 @@ class BufferTest extends PHPUnit_Framework_TestCase {
         $this->assertNotEmpty( $test->id, "flushind the buffer should save to db and update auto increment column on model" );
 
         $test->delete();
+
+        $buffer = new Buffer(0, Test::class);
+        $this->assertFalse($buffer->flush(), "flushing a zero sized buffer should fail");
+    }
+
+    public function test_set_insert_ignore () {
+
+        $this->buffer->set_insert_ignore(true);
+        $this->assertTrue($this->buffer->get_insert_ignore());
+
+        $this->buffer->set_insert_ignore(1);
+        $this->assertTrue($this->buffer->get_insert_ignore());
+
+        $this->buffer->set_insert_ignore(0);
+        $this->assertFalse($this->buffer->get_insert_ignore());
     }
 
 }
