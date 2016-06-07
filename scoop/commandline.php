07a0fa60075fa00cbcf3d7af2fc943fdce445509
justinrobinson <justinrobinson@bizjournals.com>
2016-06-07 14:49:59 -0400

    [php56] refactor command line

--- scoop/commandline.php
+++ scoop/commandline.php
@@ -13,84 +13,49 @@ class CommandLine {
 
     public static $args;
 
+    public static $numArgs;
+
     /**
      * @param array|null $argv
      *
      * @return array
      */
-    public static function parseArgs ( array $argv = null ) {
+    public static function parse_args ( array $argv = null ) {
 
         $argv = $argv ? $argv : $_SERVER['argv'];
 
         // first element is the script name
         array_shift ( $argv );
 
-        $numArgs = count ( $argv );
+        self::$numArgs = count ( $argv );
 
-        $out = [ ];
+        self::$args = [];
 
-        for ( $i = 0, $j = $numArgs; $i < $j; $i++ ) {
-            $arg = $argv[$i];
+        foreach ( $argv as $argIndex => $arg ) {
+
+            if ( !array_key_exists($argIndex, $argv) ) {
+                continue;
+            }
 
             // --foo --bar=baz
             if ( substr ( $arg, 0, 2 ) === '--' ) {
-                $eqPos = strpos ( $arg, '=' );
-
-                // --foo
-                if ( $eqPos === false ) {
-                    $key = substr ( $arg, 2 );
-
-                    // --foo value
-                    if ( $i + 1 < $j && $argv[$i + 1][0] !== '-' ) {
-                        $value = $argv[$i + 1];
-                        $i++;
-                    } else {
-                        $value = isset( $out[$key] ) ? $out[$key] : true;
-                    }
-                    $out[$key] = $value;
-                } // --bar=baz
-                else {
-                    $key = substr ( $arg, 2, $eqPos - 2 );
-                    $value = substr ( $arg, $eqPos + 1 );
-                    $out[$key] = $value;
-                }
-            } // -k=value -abc
+                self::parse_long_opt($argv, $argIndex);
+            }
             else if ( substr ( $arg, 0, 1 ) === '-' ) {
-                // -k=value
-                if ( substr ( $arg, 2, 1 ) === '=' ) {
-                    $key = substr ( $arg, 1, 1 );
-                    $value = substr ( $arg, 3 );
-                    $out[$key] = $value;
-                } // -abc
-                else {
-                    $chars = str_split ( substr ( $arg, 1 ) );
-                    foreach ( $chars as $char ) {
-                        $key = $char;
-                        $value = isset( $out[$key] ) ? $out[$key] : true;
-                        $out[$key] = $value;
-                    }
-                    // -a value1 -abc value2
-                    if ( $i + 1 < $j && $argv[$i + 1][0] !== '-' ) {
-                        $out[$key] = $argv[$i + 1];
-                        $i++;
-                    }
-                }
-            } // plain-arg
+                self::parse_short_opt( $argv, $argIndex );
+            }
             else {
-                $value = $arg;
-                $out[] = $value;
+                self::parse_opt($argv, $argIndex);
             }
         }
 
-        self::$args = $out;
-
-        return $out;
+        return self::$args;
     }
 
     /**
      * GET BOOLEAN
      */
-    public static function getBoolean ( $key, $default = false ) {
+    public static function get_boolean ( $key, $default = false ) {
 
         if ( !isset( self::$args[$key] ) ) {
             return $default;
@@ -125,5 +90,75 @@ class CommandLine {
         }
 
         return $default;
+    }
+
+    /**
+     * @param $argv
+     * @param $index
+     */
+    private static function parse_long_opt ( &$argv, $index ) {
+
+        $arg = $argv[$index];
+        $nextIndex = $index + 1;
+
+        $eqPos = strpos ( $arg, '=' );
+
+        // --foo
+        if ( $eqPos === false ) {
+            $key = substr ( $arg, 2 );
+
+            // --foo value
+            if ( $nextIndex < self::$numArgs && $argv[$nextIndex][0] !== '-' ) {
+                $value = $argv[$nextIndex];
+                unset($argv[$nextIndex]);
+            } else {
+                $value = isset( self::$args[$key] ) ? self::$args[$key] : true;
+            }
+        } // --bar=baz
+        else {
+            $key = substr ( $arg, 2, $eqPos - 2 );
+            $value = substr ( $arg, $eqPos + 1 );
+        }
+
+        self::$args[$key] = $value;
+    }
+
+    /**
+     * @param $argv
+     * @param $index
+     */
+    private static function parse_short_opt ( &$argv, $index ) {
+
+        $arg = $argv[$index];
+        $nextIndex = $index+1;
+
+        // -k=value
+        if ( substr ( $arg, 2, 1 ) === '=' ) {
+            $key = substr ( $arg, 1, 1 );
+            $value = substr ( $arg, 3 );
+        } // -abc
+        else {
+            $chars = str_split ( substr ( $arg, 1 ) );
+            foreach ( $chars as $char ) {
+                $key = $char;
+                $value = isset( $out[$key] ) ? $out[$key] : true;
+            }
+            // -a value1 -abc value2
+            if ( $nextIndex < self::$numArgs && $argv[$nextIndex][0] !== '-' ) {
+                $value = $argv[$nextIndex];
+                unset($argv[$nextIndex]);
+            }
+        }
+
+        self::$args[$key] = $value;
+    }
+
+    /**
+     * @param $argv
+     * @param $index
+     */
+    private static function parse_opt ( &$argv, $index ) {
+
+        self::$args[] = $argv[$index];
     }
 }
